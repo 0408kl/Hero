068cd4d13302fdd9ea245f8bb42ed7ed927ce963
Késsia Laura Dias Paulina <2016118050211@ifpi.int>
2018-12-03 16:21:36 -0300

    Método onSelect() criado

--- client/src/app/heroes/heroes.component.ts
+++ client/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 
  @Component({
   selector: 'app-heroes',
@@ -7,8 +8,12 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  };
   constructor() { }
    ngOnInit() {
    }
